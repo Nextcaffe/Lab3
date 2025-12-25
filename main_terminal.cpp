80ae83488caaa8a4d6707034e458837c57a0f238
Nextcaffe <147265523+Nextcaffe@users.noreply.github.com>
2025-12-25 21:31:06 +0300

    Add files via upload
    new mains

---
+++ main_terminal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include <string>
+#include <vector>
+#include "File_interaction.h"
+
+int main(int argc, char ** argv)
+{
+    bool add_separator = false;
+    std::string TypeOfMethod, password, TypeOfOperation, input_message = "";
+    TypeOfMethod = argv[1];
+    password = argv[2];
+    TypeOfOperation = argv[3];
+    
+    for (int i = 4; i < argc; i++) {
+        input_message += argv[i];
+    }
+
+    auto output_message = transformation_message(TypeOfMethod, password, TypeOfOperation, input_message);
+
+    if (TypeOfOperation == "1") {
+        std::cout << "Encrypted message:" << std::endl;
+    } else if (TypeOfOperation == "2") {
+        std::cout << "Decrypted message:" << std::endl;
+    } else {
+        throw std::runtime_error("Unknown operation");
+    }
+    
+    for (size_t i = 0; i < output_message.size(); i++) {
+            std::cout << std::hex << static_cast<int>(output_message[i]) << std::setw(add_separator);
+        }
+    std::cout << std::endl;
+
+}
