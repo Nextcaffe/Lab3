30cfc148e8b9765534867189842d60e5f0252f5e
Nextcaffe <147265523+Nextcaffe@users.noreply.github.com>
2025-12-25 21:07:07 +0300

    Update File_interaction.h
    delete initialization function of another h file

--- File_interaction.h
+++ File_interaction.h
@@ -8,7 +8,6 @@
 using Data = std::vector<uint8_t>;
 using Text = const std::string&;
 
-CryptoType parseCryptoType(const std::string& type);
 Data transformation_message(Text TypeOfMethod, Text password, Text TypeOfOperation, Text input_message);
 
 #endif //FILE_INTERACTION_H
